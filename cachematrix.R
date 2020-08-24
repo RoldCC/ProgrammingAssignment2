b2b6d1b287559eb56193df8fe76548ffd52a2dca
RoldCC <rochac1902@gmail.com>
2020-08-24 12:59:39 -0600

    Test

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,23 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(g){
+                if (nrow(g)!=ncol(g)){
+                        return("The matrix need to be squared")
+                }
+                else{
+                        x <<- g
+                        inv <<- NULL
+                }
+        }
+        get <- function() x
+        setinv <- function(m) inv <<- m
+        getinv <- function() inv
+        list(set = set,
+             get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
@@ -12,4 +28,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv_mat <- x$getinv()
+        if (!is.null(inv_mat)){
+                message("obtaining the inverse...")
+                return(inv_mat)
+        }
+        else {
+                info <- x$get()
+                calc_inv <- solve(info)
+                x$setinv(calc_inv)
+                calc_inv
+        }
 }
